0c286fdff6f381f3705ff08202e8026d77cb570f
khan <khandv@gmail.com>
2020-04-24 16:39:53 +0300

    py_basic_use-1.5

---
+++ py_basic_use/test.py
@@ -0,0 +1,23 @@
+class A:
+    val = 1
+
+    def foo(self):
+        A.val += 2
+
+    def bar(self):
+        self.val += 1
+
+
+a = A()
+b = A()
+
+a.bar()
+print(a.val)
+a.foo()
+print(a.val)
+
+c = A()
+print()
+print(a.val)
+print(b.val)
+print(c.val)
