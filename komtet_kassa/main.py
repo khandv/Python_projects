0bf3b8c41d1c1eeb838c77c6e6d3757937066436
khandv@gmail.com <wbrkJnhjY75>
2021-12-07 21:43:20 +0300

    3 commit

--- komtet_kassa/main.py
+++ komtet_kassa/main.py
@@ -1,7 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+import json
 
 
 def main(name = input('Введите номер заказа: ')):
@@ -9,8 +7,7 @@ def main(name = input('Введите номер заказа: ')):
     print(f'Заказ1 №: {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
